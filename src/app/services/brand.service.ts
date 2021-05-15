780d7269d62ed18878a7209c0603b172e24a4647
Talha Saglam <saglam_talha@hotmail.com>
2021-05-15 20:20:56 +0300

    markalara göre listeleme

---
+++ src/app/services/brand.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/ListResponseModel';
+import { Brand } from '../models/brand';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BrandService {
+
+  apiUrl = 'https://localhost:44380/api/brands/getall';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getBrands():Observable<ListResponseModel<Brand>> {
+    return this.httpClient.get<ListResponseModel<Brand>>(this.apiUrl);
+  }
+}
--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { CarResponseModel } from '../models/carResponseModel';
 import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/ListResponseModel';
+import { Car } from '../models/car';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
 
-  apiUrl = 'https://localhost:44380/api/cars/getall';
+  apiUrl = 'https://localhost:44380/api/';
 
   constructor(private httpClient: HttpClient) { }
 
-  getCars():Observable<CarResponseModel> {
-    return this.httpClient.get<CarResponseModel>(this.apiUrl);
+  getCars():Observable<ListResponseModel<Car>> {
+    let  newpPath = this.apiUrl +"cars/getall"
+    return this.httpClient.get<ListResponseModel<Car>>(newpPath);
+  }
+
+  getCarsByBrand(branId:number):Observable<ListResponseModel<Car>> {
+    let  newpPath = this.apiUrl +"cars/getbybrand?id="+branId
+    return this.httpClient.get<ListResponseModel<Car>>(newpPath);
   }
 }
