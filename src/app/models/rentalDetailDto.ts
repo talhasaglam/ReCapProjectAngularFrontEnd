2f89017f54add6042e9bcc8492ccdf486be81717
Talha Saglam <saglam_talha@hotmail.com>
2021-05-23 21:05:54 +0300

    Colors and rentals listed

---
+++ src/app/models/rentalDetailDto.ts
@@ -0,0 +1,11 @@
+export interface RentalDetail{
+
+    customerName:string;
+    customerLastName:string;
+    customerCompanyName:string;
+    brandName:string;
+    colorName:string;
+    dailyPrice:number;   
+    rentDate:Date;
+    returnDate:Date;
+}
